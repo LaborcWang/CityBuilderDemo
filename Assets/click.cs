98d13282b7a38195c48e1726c71c024a74411f70
UAD\1705118 <1705118@uad.ac.uk>
2019-05-21 11:38:05 +0100

    no message

---
+++ Assets/click.cs
@@ -0,0 +1,64 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click : MonoBehaviour {
+    public GameObject redcube;
+    public ray ray1_object;
+    private Camera _currentCamera;
+    private Transform _target;
+    private void Awake()
+    {
+        _currentCamera = Camera.main;
+        _target = transform;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 CO_Direction = _target.position - _currentCamera.transform.position;
+        float cPlane = Vector3.Dot(CO_Direction, _currentCamera.transform.forward);
+        Ray cameraRay = _currentCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(cameraRay,out hit, float.PositiveInfinity, ~LayerMask.GetMask("Cube")))
+        {
+            print(hit.collider.name);
+            _target.position = hit.point + Vector3.up * 0.5f;
+        }
+        else
+        {
+            _target.position = _currentCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        }
+        return;
+        //得到摄像机到物体的向量
+        
+        //Vector3 CO_Direction = _target.position - _currentCamera.transform.position;
+        //得到摄像机与物体所在平面的距离
+        //float cPlane = Vector3.Dot(CO_Direction, _currentCamera.transform.forward);
+        _target.position = _currentCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cPlane));
+        if (ray1_object.HitInfo.collider!=null)
+        {
+            _target.position = ray1_object.HitInfo.point + Vector3.up * 0.5f;
+        }
+        else
+        {
+            _target.position = _currentCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cPlane));
+        }
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 0);
+        //clone.transform.position = mousepos;
+    }
+    public void OnMouseDown()
+    {
+        GameObject clone = Instantiate(redcube, redcube.transform.position, Quaternion.identity);
+        //Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 0);
+        //clone.transform.position = mousepos;
+    }
+}
---
+++ Assets/ray.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ray : MonoBehaviour
+{
+
+    public Material m1;
+    public RaycastHit HitInfo;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckBarrier();
+    }
+    void CheckBarrier()
+    {
+
+        //创建一个射线 关键字Ray
+
+        //第一个参数放的是发射射线的物体的位置，第二个参数放的是发射射线的方向
+
+        Ray ray1 = new Ray(this.transform.position, -this.transform.up);
+
+        //发射射线
+
+        
+
+        //射线默认长度为无穷大，想要设置射线长度加一个参数即可，例如设置射线长度为五（ray，out HitInfo，5）
+
+        bool result = Physics.Raycast(ray1, out HitInfo);
+
+        Debug.DrawRay(ray1.origin, ray1.direction,Color.red);
+
+        //判断射线是否碰到物体，碰到物体打印碰撞到的物体的名字
+
+        if (result)
+        {
+
+            //Debug.Log(HitInfo.collider.name);
+            HitInfo.collider.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+}
